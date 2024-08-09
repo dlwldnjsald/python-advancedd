ead949dbb501fc7bd39ec97619788b98768757d7
dlwlndjsald <dlwlndjsald@gmail.com>
2024-08-09 17:01:56 +0900

    0809 point, point_test

--- point.py
+++ point.py
@@ -1,14 +1,81 @@
+
 class Point:
 
     instance_count = 0 # 클래스 멤버
     # 클래스 멤버 : 클래스 이름 공간 내에 생성, 모든 인스턴스 객체가 공유
     # 인스턴스 멤버 : 인스턴스 공간 내에 생성, 개별 인스턴스 객체 내부에서만 활용
 
-    #setter
+
+    # init & del
+    def __init__(self, x=0, y=0): #매개변수 x,y
+        self.x, self.y = x, y
+        Point.instance_count += 1
+
+    def __del__(self):
+        Point.instance_count -= 1
+
+
+    # str & repr
+    def __str__(self):
+        # 객체를 문자열로 변환
+        return f"Point x = {self.x}, y = {self.y}"
+    def __repr__(self):
+        # 객체를 문자열로 변환 ->
+        # 원래 목적: 객체 재현
+        return f"Point({self.x}, {self.y})"
+
+
+    # 연산자 오버로딩
+
+    # 산술 연산자 + 오버로딩
+    # Point + Point
+    # Point + int
+    def __add__(self, other): # Point + {other}
+        if isinstance(other, int): # x,y 값에 int 값을 합산
+            self.x += other
+            self.y += other
+        elif isinstance(other, Point):
+            self.x += other.x
+            self.y += other.y
+        return self
+
+    # 역이항 + 산술연산자
+    def __radd__(self, other): # {other} + Point
+        if isinstance(other, int):
+            self.x += other
+            self.y += other
+        return self
+
+    # 산술 연산자 - 오버로딩
+    # Point - Point
+    # Point - int
+    def __sub__(self, other):  # Point - {other}
+        if isinstance(other, int):  # other-> int일경우
+            self.x -= other
+            self.y -= other
+        elif isinstance(other, Point):  # other-> Point일 경우
+            self.x -= other.x
+            self.y -= other.y
+        return self
+
+    # 역이항 - 산술연산자
+    def __rsub__(self, other):  # {other} - Point
+        if isinstance(other, int):
+            self.x -= other
+            self.y -= other
+        return self
+
+    # 비교 연산자
+    def __eq__(self, other): # Point == {other}
+        if (isinstance(other, Point)):
+            return self.x == other.x and \
+                    self.y == other.y
+        return False
+
+    # setter & getter
     def setX(self, x):  # 인스턴스 메서드의 첫번째 인자는 self
         self.x = x
 
-    #getter
     def getX(self):
         return self.x
 
@@ -18,3 +85,57 @@ class Point:
     def getY(self):
         return self.y
 
+
+# 정적 메서드와 클래스 메서드
+# - 클래스 메서드 : 모든 인스턴스들이 공유하는 메서드
+# - 정적 메서드: 현재 클래스의 네임스페이스만 공유
+# @staticmethod, @classmethod라는 데코레이터로 선언 가능
+# Singleton Pattern 예제 ->
+
+class Singleton:
+    # 인스턴스를 저장할 객체
+    __instance = None
+
+
+    def __init__(self):
+        # 유일 객체만 유지해야
+        if Singleton.__instance != None:
+            raise Exception("이 객체는 싱글턴입니다. 유일 객체만 유지해야되요 use getInstance()")
+        else:
+            Singleton.__instance = self
+
+
+    # 클래스 메서드
+    @classmethod
+    def getClassInstance(cls):
+        print(cls.__instance)
+        if cls.__instance == None:
+            Singleton()
+        return cls.__instance
+
+
+    # 정적 메서드
+    @staticmethod
+    def getInstance():
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+class MyNumber:
+    def __init__(self, value):
+        self.value = value
+
+    def __iadd__(self, other):
+        self.value += other.value
+        return self
+
+    def __isub__(self, other):
+        self.value -= other.value
+        return self
+
+    def __iand__(self, other):
+        self.value &= other.value
+        return self
+
+    def __repr__(self):
+        return f"비트연산자 &=: {self.value}"
--- point_test.py
+++ point_test.py
@@ -1,4 +1,5 @@
 from point import Point
+from point import MyNumber
 
 def bound_instance_method():
     """
@@ -49,9 +50,103 @@ def class_member_test():
     print(p1.x is p2.x)
     print(p1.instance_count is p2.instance_count) # is 같은객체
 
+def constructor_test():
+    p1 = Point(10,20)
+    print("instance_count:", Point.instance_count)
+    p2 = Point()
+    print("instance_count:", Point.instance_count)
+    p3 = Point(x = 30, y = 40)
+    print("instance_count:", Point.instance_count)
+
+    del p1
+    del p2
+
+    print("instance_count:", Point.instance_count)
+
+def stringify():
+    p = Point(10, 20)
+    print(p)    # 문자열화 -> __str__이 호출
+    print(repr(p)) # 문자열화 -> 원래 객체를 재현할 수 있는 문자열
+
+    p_repr = eval(repr(p))
+    print(p_repr) # 똑같은 원래 객체를 재현 가능
+
+    print(p.x == p_repr.x)
+    print(p.y == p_repr.y)
+
+
+from point import Singleton
+
+def singleton_test():
+    s = Singleton()
+    print(s)
+
+    #s2 = Singleton()
+    s2 = Singleton.getClassInstance()
+    print(s2)
+
+    print(s is s2) # True
+
+
+def oper_overriding():
+    print("++++++++++++++++++++++++++++")
+    # 산술연산자 + 오버로딩
+    print("산술연산자 +:", Point(10,20) + Point(30, 40))
+    print("산술연산자 +:", Point(10,20) + 30)
+
+    # 역이항 + 산술연산자 오버로딩
+    print("역이항 r+:", Point(30, 40) + Point(10, 20))
+    print("역이항 r+:", 30 + Point(10,20))   #__radd__ => {other} + Point 추가 설정 필요
+
+
+    print("-----------------------------")
+    # 산술연산자 - 오버로딩
+    print("산술연산자 -:", Point(10, 20) - Point(30, 40))
+    print("산술연산자 -:", Point(10, 20) - 30)
+
+    # 역이항 - 산술연산자 오버로딩
+    print("역이항 r-:", 30 - Point(10,20))
+
+    print("<<<<<<>>>>>><<<<<<>>>>>>")
+    # 비교연산자
+    print("비교연산자:", Point(10,20) == Point(10,20))
+    print("비교연산자:", Point(10,20) == Point(30,40))
+
+    print("+=+=+=+=+=++=+=+=+=+=+=")
+    # 확장 연산자 +=
+    a = MyNumber(10)
+    b = MyNumber(5)
+    a += b
+    print("확장 연산자 +=:", a.value)  # 출력: 15
+
+    print("-=-=-=-=-=-=-=-=--=-=-=-=")
+    # 확장 연산자 -=
+    a = MyNumber(3544634)
+    b = MyNumber(43562345)
+    a -= b
+    print("확장 연산자 -=:", a.value)
+
+    print("&=&=&=&=&=&=&=&=&=&=&=&=&=&=")
+    # 비트 연산자 &=
+    # 두 숫자의 비트 AND 연산을 수행한 후 결과를 왼쪽 변수에 할당
+    a = MyNumber(12) # 12는 1100(2진수)
+    b = MyNumber(10) # 10은 1010 (2진수)
+    a &= b
+    print(a)  # 출력: BitwiseAnd(8), 8은 1000 (2진수)
+
+
+
+    # 연습
+    # 산술 연산자, 역이항 연산자, 비교 연산자
+    # 오버라이딩 연습해보기
+
 
 
 if __name__ == "__main__":
     #bound_instance_method()
     #unbound_instance_method()
-    class_member_test()
+    #class_member_test()
+    #constructor_test()
+    #stringify()
+    #singleton_test()
+    oper_overriding()
